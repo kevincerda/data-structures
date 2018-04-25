b72c330da06b9aa3da0520ddea2f6f7ae0911535
Kevin <kevin@cerda.io>
2018-04-25 15:34:32 -0700

    implemented

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,29 @@
-var Queue = function() {
-  var someInstance = {};
+const Queue = function() {
+  const someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  let storage = {};
+  let dequeueCount = 0;
+  let count = 0;
+
+  // {0: 'a', 1: 'b', 2: 'c'}
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count += 1;
   };
 
   someInstance.dequeue = function() {
+     const result = storage[dequeueCount];
+     delete storage[dequeueCount];
+     dequeueCount += 1;
+     return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,28 @@
-var Stack = function() {
-  var someInstance = {};
+const Stack = function() {
+  const someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  const storage = {};
+  let count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count += 1;
   };
 
   someInstance.pop = function() {
+    if (count > 0) {
+      count -= 1;
+      const result = storage[count];
+      delete storage[count];
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
-
+  
   return someInstance;
 };
