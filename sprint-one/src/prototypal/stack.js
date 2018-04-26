74f179d22914cf50ca63da0edf16381c33b9937c
Kevin <kevin@cerda.io>
2018-04-25 21:22:37 -0700

    Implemented stack in prototypal pattern.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var obj = Object.create(stackMethods);
+	obj.storage = {};
+	obj.count = 0;
+	return obj;
 };
 
 var stackMethods = {};
+stackMethods.push = function(value) {
+	this.storage[this.count] = value;
+	this.count += 1;
+};
+stackMethods.pop = function() {
+	if (this.count > 0) {
+		this.count -= 1;
+		const result = this.storage[this.count];
+		delete this.storage[this.count];
+		return result;
+	}
+};
+stackMethods.size = function() {
+	return Object.keys(this.storage).length;
+};
 
 
