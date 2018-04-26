8e6cc6ea1532f84ebea74209ff038a8adc4b44c2
Kevin <kevin@cerda.io>
2018-04-25 21:16:11 -0700

    Implemented queue in prototypal.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var someInstance = Object.create(queueMethods);
+	someInstance.storage = {};
+	someInstance.count = 0;
+	someInstance.dequeueCount = 0;
+	return someInstance;
 };
 
 var queueMethods = {};
-
+queueMethods.enqueue = function(value) {
+	this.storage[this.count] = value;
+	this.count += 1;
+};
+queueMethods.dequeue = function() {
+	const result = this.storage[this.dequeueCount];
+	delete this.storage[this.dequeueCount];
+	this.dequeueCount += 1;
+	return result;
+};
+queueMethods.size = function() {
+	return Object.keys(this.storage).length;
+};
 
