5b57359aa7e81d26801a9500ee6c133c1d7d1f8e
Kevin <kevin@cerda.io>
2018-04-26 20:55:42 -0700

    Implemented tree.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,29 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	var node = new Tree(value);
+	this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+	var contains = false;
+	if (this.value === target) {
+		contains = true;
+	}
+	this.children.forEach(function(child) {
+		if (child.contains(target)) {
+			contains = true;
+		}
+	});
+	return contains;
 };
 
 
