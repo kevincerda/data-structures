044fdeecb6d1ff6348282cae02922761eb9c02e2
Kevin <kevin@cerda.io>
2018-04-28 12:28:03 -0700

    Implemeneted insert and contains.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,34 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
 };
+BinarySearchTree.prototype.insert = function(value) {
+  var node = new BinarySearchTree(value);
+  if (this.value > node.value) {
+    (!this.left) ? this.left = node : this.left.insert(value);
+  } else if (this.value < node.value) {
+    (!this.right) ? this.right = node : this.right.insert(value);
+  }
+};
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else if (this.left && value < this.value) {
+    if (this.left.contains(value)) {
+      return true;
+    }
+  } else if (this.right && value > this.value) {
+    if (this.right.contains(value)) {
+      return true;
+    }
+  }
+  return false;
+};
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  callback(this.value);
+};
+
 
 
 /*
