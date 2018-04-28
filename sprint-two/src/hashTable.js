1e152d4d7ce19fc372ecf4d2209e5379933846f0
Kevin <kevin@cerda.io>
2018-04-27 21:45:31 -0700

    Implemented hashTable.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+	const tuple = [k, v];
+	if (bucket) {
+		for (let i = 0; i < bucket.length; i++) {
+			if (bucket[i][0] === k) {
+				bucket[i] = tuple;
+			} else {
+			  bucket.push(tuple)
+			}
+		}
+	} else {
+		this._storage.set(index, []);
+		var bucket = this._storage.get(index);
+		bucket.push(tuple);
+	}
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  const bucket = this._storage.get(index);
+  const bucketVal = bucket.filter(function(tuple) {
+  	return tuple.includes(k);
+  });
+  return bucketVal.length > 0 ? bucketVal[0][1] : undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  const bucket = this._storage.get(index);
+  const newBucket = bucket.filter(function(tuple) {
+  	return (!tuple.includes(k));
+  });
+  this._storage.set(index, newBucket);
 };
 
 
