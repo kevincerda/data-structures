0f38553022e37367311a2073ec3ffabeb226bd9e
Kevin <kevin@cerda.io>
2018-04-27 21:45:46 -0700

    Implemented set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	if (!this._storage.includes(item)) {
+		this._storage.push(item);
+	}
 };
 
 setPrototype.contains = function(item) {
+	return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
-};
+	for (let i = 0; i < this._storage.length; i += 1) {
+		if (this._storage[i] === item) {
+			this._storage.splice(i, 1);
+		}
+	}
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
